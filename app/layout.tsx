b87202fd743c10c44c319524e80e5121cfe6f16c
JnrMls <bladimirm2008@gmail.com~>
2026-09-09 15:57:57 -0500

    ft/ interface task added

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export const metadata: Metadata = {
   description: "A to-do list application",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
       lang="en"
